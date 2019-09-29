5d113fb4dd67646820fb9b52edb6ea36aba1453d
Emad Abd Elhamid <emadabel@yahoo.com>
2019-09-30 01:34:22 +0200

    Validating the inputs and send it to the second activity

--- Day-2-of-100/app/src/main/java/com/example/day2of100/ActivityB.java
+++ Day-2-of-100/app/src/main/java/com/example/day2of100/ActivityB.java
@@ -1,13 +1,31 @@
 package com.example.day2of100;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityB extends AppCompatActivity {
+
+    private TextView mTextViewEmail;
+    private TextView mTextViewPassword;
+    String mGetExtraEmail;
+    String mGetExtraPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        mTextViewEmail = findViewById(R.id.text_view_email);
+        mTextViewPassword = findViewById(R.id.text_view_password);
+
+        Intent intentActivityB = getIntent();
+        if (intentActivityB != null) {
+            mGetExtraEmail = intentActivityB.getStringExtra(MainActivity.EXTRA_EMAIL);
+            mGetExtraPassword = intentActivityB.getStringExtra(MainActivity.EXTRA_PASSWORD);
+            mTextViewEmail.setText(mGetExtraEmail);
+            mTextViewPassword.setText(mGetExtraPassword);
+        }
     }
 }
--- Day-2-of-100/app/src/main/java/com/example/day2of100/MainActivity.java
+++ Day-2-of-100/app/src/main/java/com/example/day2of100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.day2of100;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_EMAIL = "extra_email";
+    public static final String EXTRA_PASSWORD = "extra_password";
 
     private EditText mEditTextEmail;
     private EditText mEditTextPassword;
@@ -22,18 +25,38 @@ public class MainActivity extends AppCompatActivity {
         mEditTextPassword = findViewById(R.id.edit_text_password);
         mButtonLogin = findViewById(R.id.button_login);
 
-        String email = mEditTextEmail.getText().toString();
-        String password = mEditTextPassword.getText().toString();
-
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate();
-            }
-
-            private void validate() {
-
+                if (validate()) {
+                    Intent intentActivityB = new Intent(MainActivity.this, ActivityB.class);
+                    intentActivityB.putExtra(EXTRA_EMAIL, mEditTextEmail.getText().toString());
+                    intentActivityB.putExtra(EXTRA_PASSWORD, mEditTextPassword.getText().toString());
+                    startActivity(intentActivityB);
+                }
             }
         });
+    }
+
+    private boolean validate() {
+        String email = mEditTextEmail.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+        boolean valid = true;
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEditTextEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            mEditTextEmail.setError(null);
+        }
+
+        if (password.isEmpty()) {
+            mEditTextPassword.setError("password can not be empty");
+            valid = false;
+        } else {
+            mEditTextPassword.setError(null);
+        }
+
+        return valid;
     }
 }
