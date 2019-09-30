be41c79213566add87365626543c65e578965b5b
Emad Abd Elhamid <emadabel@yahoo.com>
2019-09-30 18:34:06 +0200

    Populate the spinner data and show selected item when press the button

--- Day-3-of-100/app/src/main/java/com/example/day3of100/MainActivity.java
+++ Day-3-of-100/app/src/main/java/com/example/day3of100/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.day3of100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner mSpinner;
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSpinner = findViewById(R.id.spinner);
+        mTextView = findViewById(R.id.textView);
+        mButton = findViewById(R.id.button);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_spinner, android.R.layout.simple_spinner_item);
+        mSpinner.setAdapter(adapter);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSpinner.getSelectedItem().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "You should select an option", Toast.LENGTH_SHORT).show();
+                } else {
+                    mTextView.setText(mSpinner.getSelectedItem().toString());
+                }
+            }
+        });
     }
 }
