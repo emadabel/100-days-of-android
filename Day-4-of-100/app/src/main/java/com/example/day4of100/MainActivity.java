97754a857188b775e6ca455e8aeb66768f8516a6
Emad Abd Elhamid <emadabel@yahoo.com>
2019-10-01 18:51:43 +0200

    Make text visible in SeekBar range 40-45

--- Day-4-of-100/app/src/main/java/com/example/day4of100/MainActivity.java
+++ Day-4-of-100/app/src/main/java/com/example/day4of100/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.day4of100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekBar;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = findViewById(R.id.seekBar);
+        mTextView = findViewById(R.id.textView);
+
+        mTextView.setVisibility(View.INVISIBLE);
+
+        mSeekBar.setMax(100);
+        mSeekBar.setProgress(0);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress < 40 || progress > 45) {
+                    mTextView.setVisibility(View.INVISIBLE);
+                } else {
+                    mTextView.setVisibility(View.VISIBLE);
+                    mTextView.setText("SeekBar value: " + progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
