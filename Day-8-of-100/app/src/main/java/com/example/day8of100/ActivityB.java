64dcc66387acd6e2c731b21f052d8a7ac8bad0e1
Emad Abd Elhamid <emadabel@yahoo.com>
2019-10-05 15:42:08 +0200

    Creating views

---
+++ Day-8-of-100/app/src/main/java/com/example/day8of100/ActivityB.java
@@ -0,0 +1,13 @@
+package com.example.day8of100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
