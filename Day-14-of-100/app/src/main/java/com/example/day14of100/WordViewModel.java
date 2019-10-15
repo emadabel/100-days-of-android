decf51b3337fb238252d577b85d4e5562f873fff
Emad Abd Elhamid <emadabel@yahoo.com>
2019-10-15 14:27:44 +0200

    Feat: Create ViewModel

---
+++ Day-14-of-100/app/src/main/java/com/example/day14of100/WordViewModel.java
@@ -0,0 +1,29 @@
+package com.example.day14of100;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
