f0b7727b6fc482fbe1303bf2579f519f5260e7f5
Emad Abd Elhamid <emadabel@yahoo.com>
2019-10-18 11:10:39 +0200

    Create translation layout

---
+++ Day-15-of-100/app/src/main/java/com/example/day15of100/TextTranslationActivity.java
@@ -0,0 +1,14 @@
+package com.example.day15of100;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextTranslationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_translation);
+    }
+}
